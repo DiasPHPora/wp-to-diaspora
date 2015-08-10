53e8a7c083e748f9b2d6aadf3b1776839f8c9517
Armando Lüscher <armando@noplanman.ch>
2015-08-10 17:02:35 +0200

    Expand post class with individual meta variables. Make access to post variables easier.

--- lib/class-post.php
+++ lib/class-post.php
@@ -14,14 +14,71 @@ class WP2D_Post {
    *
    * @var WP_Post
    */
-  private $_wp_post = null;
+  public $post = null;
 
   /**
-   * The post's custom meta data.
+   * The original post ID.
+   *
+   * @var int
+   */
+  public $ID = null;
+
+  /**
+   * If this post should be shared on diaspora*.
+   *
+   * @var bool
+   */
+  public $post_to_diaspora = null;
+
+  /**
+   * If a link back to the original post should be added.
+   *
+   * @var bool
+   */
+  public $fullentrylink = null;
+
+  /**
+   * What content gets posted.
+   *
+   * @var string
+   */
+  public $display = null;
+
+  /**
+   * The types of tags to post. (global,custom,post)
    *
    * @var array
    */
-  private $_meta = null;
+  public $tags_to_post = null;
+
+  /**
+   * The post's custom tags.
+   *
+   * @var array
+   */
+  public $custom_tags = null;
+
+  /**
+   * Aspects this post gets posted to.
+   *
+   * @var array
+   */
+  public $aspects = null;
+
+  /**
+   * Services this post gets posted to.
+   *
+   * @var array
+   */
+  public $services = null;
+
+
+  /**
+   * The post's history of diaspora* posts.
+   *
+   * @var array
+   */
+  public $post_history = null;
 
   /**
    * Setup all the necessary WP callbacks.
@@ -47,8 +104,33 @@ class WP2D_Post {
    * @param int|WP_Post $post Post ID or the post itself.
    */
   public function __construct( $post = null) {
-    // Assign the original post object.
-    $this->_wp_post = get_post( $post );
+    $this->_assign_wp_post( $post );
+  }
+
+  /**
+   * Assign the original WP_Post object and all the custom meta data.
+   *
+   * @param int|WP_Post $post Post ID or the post itself.
+   */
+  private function _assign_wp_post( $post ) {
+    if ( $this->post = get_post( $post ) ) {
+      $this->ID = $this->post->ID;
+
+      $options = WP2D_Options::get_instance();
+
+      // Assign all meta values, expanding non-existent ones with the defaults..
+      $meta = wp_parse_args(
+        get_post_meta( $this->ID, '_wp_to_diaspora', true ),
+        $options->get_options()
+      );
+      if ( $meta ) {
+        foreach ( $meta as $key => $value ) {
+          $this->$key = $value;
+        }
+      }
+
+      $this->post_history = get_post_meta( $this->ID, '_wp_to_diaspora_post_history', true );
+    }
   }
 
   /**
@@ -58,7 +140,8 @@ class WP2D_Post {
    * @param WP_Post $post    Post object being saved.
    */
   public function post( $post_id, $post ) {
-    $this->_wp_post = $post;
+    $this->_assign_wp_post( $post );
+
     $options = WP2D_Options::get_instance();
 
     // Is this post type enabled for posting?
@@ -66,23 +149,14 @@ class WP2D_Post {
       return;
     }
 
-    // Get the post's meta data.
-    $this->_meta = get_post_meta( $post_id, '_wp_to_diaspora', true );
-    if ( empty( $this->_meta ) ) {
-      return;
-    }
-
-    // Facilitate access to meta data.
-    extract( $this->_meta, EXTR_PREFIX_ALL, 'meta' );
-
     // Make sure we're posting to diaspora* and the post isn't password protected.
     // TODO: Maybe somebody wants to share a password protected post to a closed aspect.
-    if ( $meta_post_to_diaspora && 'publish' === $post->post_status && '' === $post->post_password ) {
+    if ( $this->post_to_diaspora && 'publish' === $post->post_status && '' === $post->post_password ) {
 
       $status_message = $this->_get_title_link();
 
       // Post the full post text or just the excerpt?
-      if ( 'full' === $meta_display ) {
+      if ( 'full' === $this->display ) {
         $status_message .= $this->_get_full_content();
       } else {
         $status_message .= $this->_get_excerpt_content();
@@ -99,7 +173,7 @@ class WP2D_Post {
 
       // Add services to share to via diaspora*.
       $extra_data = array(
-        'services' => $meta_services
+        'services' => $this->services
       );
 
       // Set up the connection to diaspora*.
@@ -109,7 +183,7 @@ class WP2D_Post {
         return false;
       }
 
-      if ( ! empty( $status_message ) && ! $api->last_error && $response = $api->post( $status_message, $meta_aspects, $extra_data ) ) {
+      if ( ! empty( $status_message ) && ! $api->last_error && $response = $api->post( $status_message, $this->aspects, $extra_data ) ) {
         // Save certain diaspora* post data as meta data for future reference.
         $this->_save_to_history( $response );
 
@@ -130,8 +204,8 @@ class WP2D_Post {
   private function _get_title_link() {
     return sprintf(
       '<p><b><a href="%1$s" title="permalink to %2$s">%2$s</a></b></p>',
-      get_permalink( $this->_wp_post->ID ),
-      $this->_wp_post->post_title
+      get_permalink( $this->ID ),
+      $this->post->post_title
     );
   }
 
@@ -149,7 +223,7 @@ class WP2D_Post {
     // Extract URLs from [embed] shortcodes.
     add_filter( 'embed_oembed_html', array( $this, 'embed_url' ), 10, 2 );
 
-    return apply_filters( 'the_content', $this->_wp_post->post_content );
+    return apply_filters( 'the_content', $this->post->post_content );
   }
 
   /**
@@ -162,13 +236,13 @@ class WP2D_Post {
     // 1. Custom post excerpt
     // 2. Text up to the <!--more--> tag
     // 3. Manually trimmed content
-    $post = $this->_wp_post;
-    $excerpt = $post->post_excerpt;
-    if ( '' == $excerpt ) {
-      if ( $more_pos = strpos( $post->post_content, '<!--more' ) ) {
-        $excerpt = substr( $post->post_content, 0, $more_pos );
+    $content = $this->post->post_content;
+    $excerpt = $this->post->post_excerpt;
+    if ( '' === $excerpt ) {
+      if ( $more_pos = strpos( $content, '<!--more' ) ) {
+        $excerpt = substr( $content, 0, $more_pos );
       } else {
-        $excerpt = wp_trim_words( $post->post_content, 42, '[...]' );
+        $excerpt = wp_trim_words( $content, 42, '[...]' );
       }
     }
     return '<p>' . $excerpt . '</p>';
@@ -181,8 +255,8 @@ class WP2D_Post {
    */
   private function _get_tags_to_add() {
     $options = WP2D_Options::get_instance();
-    $tags_to_post = $this->_meta['tags_to_post'];
-    $custom_tags  = $this->_meta['custom_tags'];
+    $tags_to_post = $this->tags_to_post;
+    $custom_tags  = $this->custom_tags;
     $tags_to_add  = '';
 
     // Add any diaspora* tags?l
@@ -203,7 +277,7 @@ class WP2D_Post {
       // Add post tags?
       if ( in_array( 'post', $tags_to_post ) ) {
         // Clean up the post tags.
-        $diaspora_tags += array_flip( wp_get_post_tags( $this->_wp_post->ID, array( 'fields' => 'slugs' ) ) );
+        $diaspora_tags += array_flip( wp_get_post_tags( $this->ID, array( 'fields' => 'slugs' ) ) );
       }
 
       // Get an array of cleaned up tags.
@@ -220,6 +294,7 @@ class WP2D_Post {
         $tags_to_add = implode( ' ', $diaspora_tags_clean ) . '<br />';
       }
     }
+
     return $tags_to_add;
   }
 
@@ -230,13 +305,14 @@ class WP2D_Post {
    */
   private function _get_posted_at_link() {
     $link = '';
-    if ( $this->_meta['fullentrylink'] ) {
+    if ( $this->fullentrylink ) {
       $link = sprintf( '%1$s [%2$s](%2$s "%3$s")',
         __( 'Originally posted at:', 'wp_to_diaspora' ),
-        get_permalink( $this->_wp_post->ID ),
-        $this->_wp_post->post_title
+        get_permalink( $this->ID ),
+        $this->post->post_title
       );
     }
+
     return $link;
   }
 
@@ -246,25 +322,22 @@ class WP2D_Post {
    * @param object $response Response from the API containing the diaspora* post details.
    */
   private function _save_to_history( $response ) {
-    $post    = $this->_wp_post;
-    $post_id = $post->ID;
-
-    // Get the existing post history.
-    $diaspora_post_history = get_post_meta( $post_id, '_wp_to_diaspora_post_history', true );
-    if ( empty( $diaspora_post_history ) ) {
-      $diaspora_post_history = array();
+    // Make sure the post history is an array.
+    if ( empty( $this->post_history ) ) {
+      $this->post_history = array();
     }
 
     // Add a new entry to the history.
-    $diaspora_post_history[] = array(
+    $this->post_history[] = array(
       'id'         => $response->id,
       'guid'       => $response->guid,
-      'created_at' => $post->post_modified,
-      'aspects'    => $this->_meta['aspects'],
+      'created_at' => $this->post->post_modified,
+      'aspects'    => $this->aspects,
       'nsfw'       => $response->nsfw,
       'post_url'   => $response->permalink
     );
-    update_post_meta( $post_id, '_wp_to_diaspora_post_history', $diaspora_post_history );
+
+    update_post_meta( $this->ID, '_wp_to_diaspora_post_history', $this->post_history );
   }
 
   /**
@@ -310,45 +383,38 @@ class WP2D_Post {
    * @param WP_Post $post The object for the current post.
    */
   public function meta_box_render( $post ) {
+    $this->_assign_wp_post( $post );
+
     // Add an nonce field so we can check for it later.
     wp_nonce_field( 'wp_to_diaspora_meta_box', 'wp_to_diaspora_meta_box_nonce' );
 
     // Get the default values to use, but give priority to the meta data already set.
     $options = WP2D_Options::get_instance();
 
-    // Use the default values if no meta data is set yet.
-    $post_meta = wp_parse_args(
-      get_post_meta( $post->ID, '_wp_to_diaspora', true ),
-      $options->get_options()
-    );
-
     // Make sure we have some value for post meta fields.
-    $post_meta['custom_tags'] = ( isset( $post_meta['custom_tags'] ) ) ? $post_meta['custom_tags'] : array();
-
-    // Facilitate access to meta data.
-    extract( $post_meta, EXTR_PREFIX_ALL, 'meta' );
+    $this->custom_tags = ( isset( $this->custom_tags ) ) ? $this->custom_tags : array();
 
     // If this post is already published, don't post again to diaspora*.
-    $meta_post_to_diaspora = ( 'publish' === get_post_status( $post->ID ) ) ? false : $meta_post_to_diaspora;
-    $meta_aspects          = ( ! empty( $meta_aspects )  && is_array( $meta_aspects ) )  ? $meta_aspects  : array();
-    $meta_services         = ( ! empty( $meta_services ) && is_array( $meta_services ) ) ? $meta_services : array();
+    $this->post_to_diaspora = ( 'publish' === get_post_status( $this->ID ) ) ? false : $this->post_to_diaspora;
+    $this->aspects          = ( ! empty( $this->aspects )  && is_array( $this->aspects ) )  ? $this->aspects  : array();
+    $this->services         = ( ! empty( $this->services ) && is_array( $this->services ) ) ? $this->services : array();
 
     // Have we already posted on diaspora*?
-    if ( ( $diaspora_post_history = get_post_meta( $post->ID, '_wp_to_diaspora_post_history', true ) ) && is_array( $diaspora_post_history ) ) {
-      $latest_post = end( $diaspora_post_history );
+    if ( is_array( $this->post_history ) ) {
+      $latest_post = end( $this->post_history );
       ?>
       <p><a href="<?php echo $latest_post['post_url']; ?>" target="_blank"><?php _e( 'Already posted to diaspora*.', 'wp_to_diaspora' ); ?></a></p>
       <?php
     }
     ?>
 
-    <p><?php $options->post_to_diaspora_render( $meta_post_to_diaspora ); ?></p>
-    <p><?php $options->fullentrylink_render( $meta_fullentrylink ); ?></p>
-    <p><?php $options->display_render( $meta_display ); ?></p>
-    <p><?php $options->tags_to_post_render( $meta_tags_to_post ); ?></p>
-    <p><?php $options->custom_tags_render( $meta_custom_tags ); ?></p>
-    <p><?php $options->aspects_render( $meta_aspects ); ?></p>
-    <p><?php $options->services_render( $meta_services ); ?></p>
+    <p><?php $options->post_to_diaspora_render( $this->post_to_diaspora ); ?></p>
+    <p><?php $options->fullentrylink_render( $this->fullentrylink ); ?></p>
+    <p><?php $options->display_render( $this->display ); ?></p>
+    <p><?php $options->tags_to_post_render( $this->tags_to_post ); ?></p>
+    <p><?php $options->custom_tags_render( $this->custom_tags ); ?></p>
+    <p><?php $options->aspects_render( $this->aspects ); ?></p>
+    <p><?php $options->services_render( $this->services ); ?></p>
 
     <?php
   }
